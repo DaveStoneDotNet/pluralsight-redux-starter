34c6d38ecfe08a526a91880db51834e22ed4378a
Dave Stone <daveandkim22@hotmail.com>
2017-06-10 10:43:43 -0500

    Initial App Structure : Create Header. Need to work out 'Unable to resolve path to module' issue for '.components/App'.

---
+++ src/components/app.js
@@ -0,0 +1,19 @@
+import React         from 'react';
+import { PropTypes } from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Header Here</h1>
+                { this.props.children }
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+                    children: PropTypes.object.isRequired
+                };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
+
+import React              from 'react';
+import { render }         from 'react-dom';
+import { Router }         from 'react-router';
+import { browserHistory } from 'react-router';
+
+import routes             from './routes';
+
+render(
+    <Router history={ browserHistory } routes={ routes } />, document.getElementById('app')
+);
+
